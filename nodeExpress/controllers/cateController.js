2e1bf991ddfc2386cf6da438a864990e92775b9e
zengjunwei1 <347350971@qq.com>
2020-11-16 09:13:22 +0800

    123

--- nodeExpress/controllers/cateController.js
+++ nodeExpress/controllers/cateController.js
@@ -1,46 +1,22 @@
-const { sqlConnect } = require('../utils/dbCnfig');
-var cheerio = require('cheerio');//引入cheerio
-var superagent = require('superagent');//引入superagent
-var superagentCharset = require('superagent-charset');//引入superagent-charset 
+const { sqlConnect } = require('../utils/dbCnfig');     //引入连接数据库配置文件
+var cheerio = require('cheerio');                       //引入cheerio
+var superagent = require('superagent');                 //引入superagent
+var superagentCharset = require('superagent-charset');  //引入superagent-charset 
 superagentCharset(superagent);
 
-module.exports={ 
-    // 接口一：获取封面轮播图图片
-    getAllcates:(req, res)=>{ 
-        var sql = "select * from dataOutside";
-        var sqlArr = [];
+module.exports={   
+    // webSite数据提交接口
+    saveWebSite:(req,res)=>{   
+        let webName = req.body.webName
+        let webUrl = req.body.webUrl
+        let webLogo = req.body.webLogo
+        let webDesc = req.body.webDesc
+        let webType = req.body.webType 
+        var sql = "INSERT INTO webSiteList (webName,webUrl,webLogo,webDesc,webType) VALUES (?,?,?,?,?)";
+        var sqlArr = [webName,webUrl,webLogo,webDesc,webType];
         var callback = (err,data)=>{
         if(err){
-            console.log('数据库连接异常');  
-            return
-        }else{ 
-            //返回数据
-            res.status(200).json({
-                code:'0000',
-                msg:'成功',
-                data:{ 
-                    Catelist:data
-                }
-            })
-        }
-        } 
-        sqlConnect(sql,sqlArr,callback)
-    },
-
-    // 接口二、家用宝存储数据
-    saveHomeLoan:(req,res)=>{   
-        let payName = req.body.payName
-        let colName = req.body.colName
-        let payMoney = req.body.payMoney
-        let payWay = req.body.payWay
-        let payDate = req.body.payDate
-        let other = req.body.other
-        var sql = "INSERT INTO SaveHomeLoan (payName,colName,payMoney,payWay,payDate,other) VALUES (?,?,?,?,?,?)";
-        var sqlArr = [payName,colName,payMoney,payWay,payDate,other];
-        var callback = (err,data)=>{
-        if(err){
-            console.log('数据库连接异常');  
-            return
+            return res.json({ code: 400, msg: err});
         }else{  
             //返回数据
             res.status(200).json({
@@ -52,48 +28,36 @@ module.exports={
         sqlConnect(sql,sqlArr,callback)
     },
 
-    //四、查询所有家用宝数据
-    getAllHomeLoan:(req,res)=>{
-        var sql = "select * from SaveHomeLoan";
+    //获取website数据
+    getWebList:(req,res)=>{
+        var sql = "select * from webSiteList";
         var sqlArr = [];
         var callback = (err,data)=>{
         if(err){
-            console.log('数据库连接异常');  
-            return
-        }else{ 
+            return res.json({ code: 400, msg: err});
+        }else{   
             //返回数据
             res.status(200).json({
                 code:'0000',
                 msg:'成功',
                 data:{ 
-                    HomeLoan:data
+                    webSiteList:data
                 }
             })
         }
         } 
         sqlConnect(sql,sqlArr,callback)
-    },
-
+    }, 
     //爬取外部数据
-    getOutData:(req, res)=>{
-        //设置请求头
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header('Access-Control-Allow-Headers', 'Content-Type');
-        //类型
-        var type = req.query.type;
-        //页码
-        var page = req.query.page;
-        type = type || 'weixin';
-        page = page || '1'; //页码
-        //源数据地址
-        var items = []; 
-        var baseUrl = "http://www.imomoe.ai/so.asp?page="+page
-        
+    getOutData:(req, res)=>{ 
+        var type = req.query.type || 'weixin';                          //类型
+        var page = req.query.page || '1';                               //页码 
+        var baseUrl = "http://www.alloyteam.com/nav/"                   //源数据地址
+        var items = [];                                                 //存放收集页面网址
+
         // 用 superagent 去抓取baseUrl的内容
         superagent.get(baseUrl)
-        .charset('gb2312')
+        .charset('utf-8') 
         .end(function (err, sres) {
             // 常规的错误处理
             if (err) {
@@ -101,27 +65,26 @@ module.exports={
             }
             // sres.text 里面存储着网页的 html 内容，将它传给 cheerio.load 之后
             // 就可以得到一个实现了 jquery 接口的变量，我们习惯性地将它命名为 `$`
-            // 剩下就都是 jquery 的内容了
-            var $ = cheerio.load(sres.text);
-            $('div.pics ul li a img ').each(function (idx, element) {
+            // 剩下就都是 jquery 的内容了  
+            var $ = cheerio.load(sres.text); 
+            $('table#oTable tbody tr').each(function (idx, element) {
                 var $element = $(element);   
-                items.push({ 
-                    id:idx,
-                    alt: $element.attr('alt'),
-                    src: $element.attr('src')
-                });
+                let td = $element.find('td').text() 
+                items.push({
+                    td:td
+                })
 
                 // 插入数据库开始
                 var sqlArr = [];  
                 //插入指令
-                var sql = `INSERT INTO dataOutside (id,alt,src) VALUE ('${idx}','${$element.attr('alt')}','${$element.attr('src')}')`;
-                var callback = (err,data)=>{
-                if(err){
-                    console.log('数据库连接异常'+err);  
-                    return
-                    }
-                } 
-                sqlConnect(sql,sqlArr,callback) 
+                // var sql = `INSERT INTO dataOutside (alt,src) VALUE (${$element.attr('alt')}','${$element.attr('src')}')`;
+                // var callback = (err,data)=>{
+                // if(err){
+                //     console.log('数据库连接异常'+err);  
+                //     return
+                //     }
+                // } 
+                // sqlConnect(sql,sqlArr,callback) 
                 //插入结束
 
             }); 
--- nodeExpress/routes/index.js
+++ nodeExpress/routes/index.js
@@ -3,9 +3,8 @@ var cateControllers = require('../controllers/cateController');
 var router = express.Router(); 
 
 //API请求接口的路由指向
-router.post('/SaveHomeLoanData', cateControllers.saveHomeLoan); //保存家用数据
-router.get('/Homelist', cateControllers.getAllHomeLoan); //获取家用宝数据
-router.get('/Catelist', cateControllers.getAllcates); //获取封面轮播图图片
+router.post('/savewebsite', cateControllers.saveWebSite); //保存网址数据信息
+router.get('/WebList', cateControllers.getWebList); //获取网址数据信息
 router.get('/OutData', cateControllers.getOutData); //爬取数据
 
 module.exports = router;
